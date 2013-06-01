7c2dc1e524b7916357a63436c53c5625c2308299
Jack Bracken <contact@jacktrick.com>
2013-06-01 22:18:42 +0100

    Lots of changes to app file. Added various basic blog routes and some small helper methods for post title and pretty printing dates

--- squeak.rb
+++ squeak.rb
@@ -2,9 +2,72 @@ require 'bundler/setup'
 require 'sinatra'
 require 'haml'
 
-#require_relative 'models/init'
+require_relative 'models/init'
+
+helpers do
+  def title
+    if @title
+      "Jack Bracken &raquo; #{@title}"
+    else
+      "Jack Bracken"
+    end
+  end
+
+  def pretty_date(time)
+    time.strftime("%^b %e, %Y - %R")
+  end
+
+  def post_show_page?
+    request.path_info =~ /\/posts\/\d+$/
+  end
+
+  def delete_post_button(post_id)
+    haml :_delete_post_button, locals: { post_id: post_id}
+  end
+end
 
 get '/' do
+  @posts = Post.order("created_at DESC").limit(10)
   haml :index
 end
 
+get "/posts/new" do
+  @title = "New Post"
+  @post = Post.new
+  haml :new_post
+end
+
+post "/posts" do
+  @post = Post.new(params[:post])
+  if @post.save
+    redirect "posts/#{@post.id}"
+  else
+    haml :new_post
+  end
+end
+
+get "/posts/:id" do
+  @post = Post.find(params[:id])
+  @title = @post.title
+  haml :post
+end
+
+get "/posts/:id/edit" do
+  @post = Post.find(params[:id])
+  @title = "Edit Form"
+  haml :edit_post
+end
+
+put "/posts/:id" do
+  @post = Post.find(params[:id])
+  if @post.update_attributes(params[:post])
+    redirect "/posts/#{@post.id}"
+  else
+    haml :edit_post
+  end
+end
+
+delete "/posts/:id" do
+  @post = Post.find(params[:id]).destroy
+  redirect "/"
+end
