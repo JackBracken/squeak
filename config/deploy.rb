5cca403424c89793fd1d8a83353e3f8c4462d031
Jack Bracken <contact@jacktrick.com>
2013-06-02 22:14:54 +0100

    Removed vlad deployment stuff

--- config/deploy.rb
+++
@@ -1,6 +0,0 @@
-require "bundler/vlad"
-set :application, "squeak"
-set :domain, "jack@jackbracken.me"
-set :deploy_to, "/home/jack/squeak"
-set :repository, 'git://github.com/JackBracken/squeak.git'
-set :revision, "master"
