ae657fde7994c7d64da0bef23647456f26e4e33d
Jack Bracken <contact@jacktrick.com>
2013-06-02 21:24:38 +0100

    Vlad deployment settings

---
+++ config/deploy.rb
@@ -0,0 +1,6 @@
+require "bundler/vlad"
+set :application, "squeak"
+set :domain, "jack@jackbracken.me"
+set :deploy_to, "/home/jack/squeak"
+set :repository, 'git://github.com/JackBracken/squeak.git'
+set :revision, "master"
